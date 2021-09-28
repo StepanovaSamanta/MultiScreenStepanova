4fb3f2ee45759570f240cbd68c8debf286036004
wsruser13 <wsruser13@local>
2021-09-28 13:47:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multiscreenstepanova/Brazilia.java
@@ -0,0 +1,14 @@
+package com.example.multiscreenstepanova;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Brazilia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brazilia);
+    }
+}
